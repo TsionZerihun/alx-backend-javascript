68285dbebb7d36456a01d74478ca6a72e56256ed
TsionZerihun <tsionzelekezerihun@gmail.com>
2023-02-20 17:03:56 +0300

    running npm install

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -5,11 +5,11 @@ export function taskFirst() {
 
 export function getLast() {
   return ' is okay';
-}  
+}
 
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
-  
+
   return combination;
-  }
+}
