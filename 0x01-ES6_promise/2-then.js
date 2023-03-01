9070e5bf6d718eb99a9ca75e68fb8c2db1e09381
TsionZerihun <tsionzelekezerihun@gmail.com>
2023-03-01 18:26:03 +0300

    task2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (promise) {
-      console.log('Got a response from the API');
-      resolve({
-        status: 200,
-        body: 'success',
-      });
-    } else reject(new Error());
-  });
+  return promise
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
