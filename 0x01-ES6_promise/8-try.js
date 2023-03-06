af49068d553cab82b4fa228c7fc09b0faae6fb09
TsionZerihun <tsionzelekezerihun@gmail.com>
2023-03-06 07:16:39 +0300

    task8

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,1 +1,7 @@
-try
+export default function divideFunction(numerator, denominator) {
+  if (denominator !== 0) {
+    return (numerator / denominator);
+  }
+
+  throw new Error('cannot divide by 0');
+}
